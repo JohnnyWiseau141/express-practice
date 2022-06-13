4e0ecbc2a960e4ab19a48f3a57d377984285b39a
JohnnyWiseau141 <john.a.foley141@gmail.com>
2022-06-13 12:48:28 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,25 @@
+//import modules
+
+import express from 'express'
+
+//create express app
+
+const app = express()
+
+//configure the app (app.set)
+
+
+// mount middleware(app.use)
+
+//mount routes
+
+app.get("/", function(req, res) {
+    res.send("<h1>Hello there general greivous!</h1>")
+})
+
+//tell the app to listen on port 3000
+
+app.listen(3000, function() {
+    console.log("Listening on port 3000")
+})
+
